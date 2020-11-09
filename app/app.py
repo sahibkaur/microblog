fb80e47c1c3fe6582643ee9b7af273abf6186347
Sahibpreet Kaur <sahibkaur81@gmail.com>
2020-11-09 23:35:53 +0530

    adds templates

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,17 @@ if __name__=="_main_":
     app.run()
 
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/index")
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
